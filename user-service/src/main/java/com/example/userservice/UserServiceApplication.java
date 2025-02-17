cc9c5314f00b7593e0968ac6b1e2d0d978ae456e
youthofmay123 <nhathuy39337343@gmail.com>
2025-02-17 23:02:26 +0700

    add: all package and create model for User'

--- user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -1,4 +1,4 @@
-package com.example.userservice.userservice;
+package com.example.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
