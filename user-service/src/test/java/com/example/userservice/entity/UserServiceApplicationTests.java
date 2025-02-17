ec0b723946a77eae0f9b0056970bd79e422de299
youthofmay123 <nhathuy39337343@gmail.com>
2025-02-17 23:02:26 +0700

    add: all package and create model for User'

--- user-service/src/test/java/com/example/userservice/entity/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/example/userservice/entity/UserServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.userservice.userservice;
+package com.example.userservice.entity;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
