d243737f77e1ee9717351062e4226b8dbc708f5a
root <root@HuyNguyen.>
2025-02-17 17:02:27 +0000

    feat: CRUD for user

---
+++ user-service/src/main/java/com/example/userservice/service/impl/UserServiceImpl.java
@@ -0,0 +1,52 @@
+package com.example.userservice.service.impl;
+
+import com.example.userservice.entity.User;
+import com.example.userservice.repository.UserRepository;
+import com.example.userservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Optional<User> getUserById(long id) {
+        return  userRepository.findById(id);
+    }
+
+    @Override
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User updateUser(Long id, User user) {
+        User existingUser = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        existingUser.setUsername(user.getUsername());
+        existingUser.setEmail(user.getEmail());
+        existingUser.setPassword(user.getPassword());
+        return userRepository.save(existingUser);
+    }
+
+    @Override
+    public User deleteUser(Long id) {
+        User existingUser = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        if(existingUser != null){
+            userRepository.deleteById(id);
+        }
+        userRepository.deleteById(id);
+        return existingUser;
+    }
+}
