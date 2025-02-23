a9ff57b6f391efbea76da0c72d1e660b2878fe24
youthofmay123 <nhathuy39337343@gmail.com>
2025-02-23 20:10:50 +0700

    feat: handle redis to save data

--- user-service/src/main/java/com/example/userservice/service/UserRedisService.java
+++ user-service/src/main/java/com/example/userservice/service/UserRedisService.java
@@ -1,4 +1,8 @@
 package com.example.userservice.service;
 
 public interface UserRedisService {
+    void save(String key, Object value, long timeout);
+    Object get(String key);
+    void delete(String key);
+
 }
