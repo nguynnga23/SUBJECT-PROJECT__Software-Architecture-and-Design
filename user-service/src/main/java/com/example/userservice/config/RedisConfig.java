04dc1914672a1e874298ab09c371bdee05a54281
youthofmay123 <nhathuy39337343@gmail.com>
2025-02-23 20:10:50 +0700

    feat: handle redis to save data

--- user-service/src/main/java/com/example/userservice/config/RedisConfig.java
+++ user-service/src/main/java/com/example/userservice/config/RedisConfig.java
@@ -1,13 +1,29 @@
 package com.example.userservice.config;
 
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 
 @Configuration
-@EnableCaching
 public class RedisConfig {
 
+    @Bean
+    public LettuceConnectionFactory lettuceConnectionFactory() {
+        return new LettuceConnectionFactory();
+    }
 
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(){
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(lettuceConnectionFactory());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new GenericJackson2JsonRedisSerializer());
+        return template;
+    }
 
 }
--- user-service/src/main/java/com/example/userservice/service/impl/UserRedisServiceImpl.java
+++ user-service/src/main/java/com/example/userservice/service/impl/UserRedisServiceImpl.java
@@ -1,4 +1,30 @@
 package com.example.userservice.service.impl;
 
-public class UserRedisServiceImpl {
+import com.example.userservice.service.UserRedisService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class UserRedisServiceImpl implements UserRedisService {
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    @Override
+    public void save(String key, Object value, long timeout) {
+        redisTemplate.opsForValue().set(key, value, timeout, TimeUnit.MINUTES);
+    }
+
+    @Override
+    public Object get(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    @Override
+    public void delete(String key) {
+        redisTemplate.delete(key);
+    }
 }
