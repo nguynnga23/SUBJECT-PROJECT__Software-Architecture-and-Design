ed52dada5a503dd86c0eecae8aa451ac1605b883
DinhNguyenChung <dinhvanchung24042003@gmail.com>
2025-02-24 14:01:51 +0700

    Building Authentication Service with JWT.

---
+++ user-service/src/main/java/com/example/userservice/service/JwtService.java
@@ -0,0 +1,31 @@
+package vn.edu.iuh.fit.user_authentication_service_jwt.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.time.Instant;
+
+@RequiredArgsConstructor
+public class JwtService {
+
+    private final String issuer;
+
+    private final Duration ttl;
+
+    private final JwtEncoder jwtEncoder;
+
+    public String generateToken(final String username) {
+        final var claimsSet = JwtClaimsSet.builder()
+                .subject(username)
+                .issuer(issuer)
+                .expiresAt(Instant.now().plus(ttl))
+                .build();
+
+        return jwtEncoder.encode(JwtEncoderParameters.from(claimsSet)).getTokenValue();
+    }
+
+}
