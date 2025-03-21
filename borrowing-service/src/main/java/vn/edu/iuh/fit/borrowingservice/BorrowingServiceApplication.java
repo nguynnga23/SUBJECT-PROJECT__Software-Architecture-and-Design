6fa6ce5d27e2700a0b293790a9b741173e33b729
DinhNguyenChung <dinhvanchung24042003@gmail.com>
2025-03-21 16:38:45 +0700

    add borrowing-service

---
+++ borrowing-service/src/main/java/vn/edu/iuh/fit/borrowingservice/BorrowingServiceApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.borrowingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BorrowingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BorrowingServiceApplication.class, args);
+    }
+
+}
