cf614aa11cf55ac27206627dd35a5816ada4a85f
Nguyen Nga <nguyennga200x@gmail.com>
2025-03-29 15:32:44 +0700

    feat: integrate Eureka client for service discovery in Borrowing Service

--- borrowing-service/src/main/java/vn/edu/iuh/fit/borrowingservice/BorrowingServiceApplication.java
+++ borrowing-service/src/main/java/vn/edu/iuh/fit/borrowingservice/BorrowingServiceApplication.java
@@ -2,8 +2,10 @@ package vn.edu.iuh.fit.borrowingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BorrowingServiceApplication {
 
     public static void main(String[] args) {
