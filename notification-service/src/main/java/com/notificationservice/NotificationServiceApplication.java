49f889d5734695631b53f70806be6d700dcfebae
Nguyen Nga <nguyennga200x@gmail.com>
2025-04-14 18:16:49 +0700

    feat: add email notification functionality and configuration properties for Kafka and MongoDB

--- notification-service/src/main/java/com/notificationservice/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/notificationservice/NotificationServiceApplication.java
@@ -1,5 +1,6 @@
 package com.notificationservice;
 
+import com.notificationservice.config.EnvLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -9,6 +10,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class NotificationServiceApplication {
 
 	public static void main(String[] args) {
+		EnvLoader.load(); // Load biến môi trường từ file .env
 		SpringApplication.run(NotificationServiceApplication.class, args);
 	}
 
