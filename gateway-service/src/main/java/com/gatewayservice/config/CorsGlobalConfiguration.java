4a612ad84404359af319f475340efb973cd28409
Nguyen Nga <nguyennga200x@gmail.com>
2025-04-22 21:15:00 +0700

    feat: add CORS configuration for the gateway service; implement CorsGlobalConfiguration class with allowed origins and methods

---
+++ gateway-service/src/main/java/com/gatewayservice/config/CorsGlobalConfiguration.java
@@ -0,0 +1,28 @@
+package com.gatewayservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+public class CorsGlobalConfiguration {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        corsConfig.setMaxAge(8000L);
+        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        corsConfig.addAllowedHeader("*");
+        corsConfig.setAllowCredentials(true); // Nếu bạn dùng cookie/JWT với credentials
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
+    }
+}
