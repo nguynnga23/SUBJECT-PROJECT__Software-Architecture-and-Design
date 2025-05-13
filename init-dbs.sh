a5bc09f5681e8e4f52f6451d3aa4d3269317401c
youthofmay123 <nhathuy39337343@gmail.com>
2025-05-13 21:53:38 +0700

    fix: gateway api

--- init-dbs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-psql -v ON_ERROR_STOP=1 --username "postgre" <<-EOSQL
-  CREATE DATABASE "user-service";
-  CREATE DATABASE "borrowing-service";
-  CREATE DATABASE "book-service";
-  CREATE DATABASE "inventory-service";
-EOSQL
