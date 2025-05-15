a18ad0baed4b4b344c949cd3d1ab624fbae9a467
Nguyen Nga <nguyennga200x@gmail.com>
2025-05-15 14:09:13 +0700

    feat: Enhance borrowing statistics with new endpoints and repository methods

--- borrowing-service/src/main/java/vn/edu/iuh/fit/borrowingservice/dto/BorrowingStatisticsDTO.java
+++ borrowing-service/src/main/java/vn/edu/iuh/fit/borrowingservice/dto/BorrowingStatisticsDTO.java
@@ -8,7 +8,9 @@ import lombok.Setter;
 public class BorrowingStatisticsDTO {
     private Long totalRequests; // Tổng số phiếu mượn
     private Long pendingRequests; // Phiếu mượn đang chờ
-    private Long canceledRequests; // Phiếu mượn đang chờ
+    private Long canceledRequests; // Phiếu mượn đã hủy
     private Long approvedRequests; // Phiếu mượn đã phê duyệt
+    private Long borrowedRequests; // Phiếu mượn đã lấy sách
+    private Long returnedRequests; // Phiếu mượn đã hoàn thành
     private Long overdueRequests; // Phiếu mượn quá hạn
 }
