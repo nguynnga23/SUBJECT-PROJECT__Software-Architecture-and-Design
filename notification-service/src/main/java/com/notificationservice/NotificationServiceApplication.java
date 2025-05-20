dab1a89d71e2f223d2aaa7fae2dc159b7cb3d171
Nguyen Nga <nguyennga200x@gmail.com>
2025-05-20 19:52:21 +0700

    feat: add library-chatbox service with Dockerfile and update environment configurations

--- notification-service/src/main/java/com/notificationservice/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/notificationservice/NotificationServiceApplication.java
@@ -1,6 +1,5 @@
 package com.notificationservice;
 
-import com.notificationservice.config.EnvLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -8,9 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class NotificationServiceApplication {
-
 	public static void main(String[] args) {
-		EnvLoader.load(); // Load biến môi trường từ file .env
 		SpringApplication.run(NotificationServiceApplication.class, args);
 	}
 
--- notification-service/src/main/java/com/notificationservice/service/NotificationService.java
+++
@@ -1,8 +0,0 @@
-package com.notificationservice.service;
-
-import com.notificationservice.entity.Notification;
-
-public interface NotificationService {
-    Notification createNotification(Notification notification);
-    Notification updateNotification(Notification notification);
-}
